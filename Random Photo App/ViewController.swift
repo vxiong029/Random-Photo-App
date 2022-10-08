e5b29a233b04aec97e44196a1fc15e70ae0cd1d6
vxiong029 <vxiong029@gmail.com>
2022-10-07 19:58:49 -0500

    Initial Commit

---
+++ Random Photo App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Random Photo App
+//
+//  Created by The Night Owl Dev on 10/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
