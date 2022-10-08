00f68cf8bad23b9d731b411c4cb221d070e08c9e
vxiong029 <vxiong029@gmail.com>
2022-10-07 20:10:58 -0500

    create imageView

--- Random Photo App/ViewController.swift
+++ Random Photo App/ViewController.swift
@@ -8,10 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.backgroundColor = .white
+        
+        return imageView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemMint
+        view.addSubview(imageView)
+        
+        imageView.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
+        imageView.center = view.center
     }
 
 
